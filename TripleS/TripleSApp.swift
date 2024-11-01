e926e47674697741879e24d5b57f50ced245a77e
Kevin Jin <skndmx@gmail.com>
2024-11-01 11:22:16 -0700

    Initial Commit

---
+++ TripleS/TripleSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripleSApp.swift
+//  TripleS
+//
+//  Created by kevin on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TripleSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripleSTests/TripleSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripleSTests.swift
+//  TripleSTests
+//
+//  Created by kevin on 11/1/24.
+//
+
+import Testing
+@testable import TripleS
+
+struct TripleSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
